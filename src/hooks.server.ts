001fdedbba76c6ad9ca921eb1c40aa80a549241a
Jökull Sólberg <jokull@solberg.is>
2023-01-20 16:18:16 +0000

    Ok lets try Toucan

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,19 +1,14 @@
-import * as SentryNode from '@sentry/node';
-import '@sentry/tracing';
 import type { HandleServerError } from '@sveltejs/kit';
-
-SentryNode.init({
-	dsn: 'https://5902fda1778e4144b54702ea77845c32@o4504226121121792.ingest.sentry.io/4504537706135552',
-	tracesSampleRate: 1.0,
-	// Add the Http integration for tracing
-	integrations: [new SentryNode.Integrations.Http()]
-});
-
-SentryNode.setTag('svelteKit', 'server');
+import { Toucan } from 'toucan-js';
 
 // use handleError to report errors during server-side data loading
 export const handleError: HandleServerError = ({ error, event }) => {
-	SentryNode.captureException(error, { contexts: { sveltekit: { event } } });
+	const sentry = new Toucan({
+		dsn: 'https://5902fda1778e4144b54702ea77845c32@o4504226121121792.ingest.sentry.io/4504537706135552',
+		request: event.request
+	});
+
+	sentry.captureException(error);
 
 	return {
 		message: error instanceof Error && 'message' in error ? error.message : String(error)
